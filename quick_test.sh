d06a8cca104acc248421364d2d60d9c82cf65caa
Michał Mnich <mm448369@students.mimuw.edu.pl>
2024-06-09 15:14:41 +0200

    fix: change port number to something more random

--- quick_test.sh
+++ quick_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 make &> /dev/null
-./kierki-serwer -f rozgrywka.txt -p 12345 &> server.log &
+./kierki-serwer -f rozgrywka.txt -p 53817 &> server.log &
 sleep 1
-./kierki-klient -h localhost -p 12345 -N -a &> clientN.log &
-./kierki-klient -h localhost -p 12345 -W -a &> clientW.log &
-./kierki-klient -h localhost -p 12345 -E -a &> clientE.log &
-./kierki-klient -h localhost -p 12345 -S -a &> clientS.log &
+./kierki-klient -h localhost -p 53817 -N -a &> clientN.log &
+./kierki-klient -h localhost -p 53817 -W -a &> clientW.log &
+./kierki-klient -h localhost -p 53817 -E -a &> clientE.log &
+./kierki-klient -h localhost -p 53817 -S -a &> clientS.log &
 wait
